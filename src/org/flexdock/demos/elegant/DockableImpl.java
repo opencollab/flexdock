46840ad04bd28e77316aab485224e57b91ac3919
Marius <no_author@no_author>
2004-08-28 15:17:34 +0000

    Added demo applications.
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@19 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/org/flexdock/demos/elegant/DockableImpl.java
@@ -0,0 +1,47 @@
+package org.flexdock.demos.elegant;
+
+import java.awt.Component;
+
+import javax.swing.JComponent;
+
+import org.flexdock.docking.DockableAdapter;
+import org.flexdock.docking.DockingManager;
+
+public class DockableImpl extends DockableAdapter {
+	private ElegantPanel panel;
+	private JComponent dragInitiator;
+	
+	public DockableImpl(ElegantPanel dockable, JComponent dragInit, String id) {
+		super(id);
+		if(dockable==null)
+			new IllegalArgumentException(
+			"Cannot create DockableImpl with a null DockablePanel.");
+		if(dragInit==null)
+			new IllegalArgumentException(
+			"Cannot create DockableImpl with a null drag initiator.");
+			
+		panel = dockable;
+		dragInitiator = dragInit;
+		DockingManager.registerDockable(this);
+	}
+
+	public Component getDockable() {
+		return panel;
+	}
+
+	public String getDockableDesc() {
+		String desc = panel.getTitle();
+		if(desc==null || desc.length()==0)
+			return "null";
+		return desc;
+	}
+
+	public Component getInitiator() {
+		return dragInitiator;
+	}
+
+	public void setDockableDesc(String desc) {
+		desc = desc==null? "": desc.trim();
+		panel.setTitle(desc);
+	}
+}
