42d18e406c3b7c6ef7af123957fe01630b49cc21
Marius <no_author@no_author>
2005-02-17 02:27:42 +0000

    This code was redundant with /core/src/java
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@60 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/org/flexdock/docking/drag/preview/DragPreview.java
+++
@@ -1,8 +0,0 @@
-package org.flexdock.docking.drag.preview;
-
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-
-public interface DragPreview {
-	public void drawPreview(Graphics2D g, Rectangle rect);
-}
