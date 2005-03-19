6c3b3c583d888a4466ae8ad6a92f98790713ae7c
Marius <no_author@no_author>
2005-03-19 19:57:53 +0000

    renamed ViewportTracker to org.flexdock.view.tracking.ViewTracker. Split event-listener functionality out into ViewListener class.
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@131 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/view/org/flexdock/view/tracking/ViewListener.java
@@ -0,0 +1,150 @@
+/*
+ * Created on Mar 18, 2005
+ */
+package org.flexdock.view.tracking;
+
+import java.awt.AWTEvent;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.KeyboardFocusManager;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashSet;
+
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.flexdock.view.View;
+
+/**
+ * @author Christopher Butler
+ */
+public class ViewListener implements PropertyChangeListener, ChangeListener, AWTEventListener {
+	private static final ViewListener SINGLETON = new ViewListener();
+	private static HashSet PROP_EVENTS = new HashSet();
+	private static final String PERMANENT_FOCUS_OWNER = "permanentFocusOwner";
+	public static final String ACTIVE_WINDOW = "activeWindow";
+	private static final String MOUSE_PRESSED = "mousePressed";
+
+	static {
+		primeImpl();
+	}
+
+	public static void prime() {
+	}
+	
+	private static void primeImpl() {
+		PROP_EVENTS.add(PERMANENT_FOCUS_OWNER);
+		PROP_EVENTS.add(ACTIVE_WINDOW);
+		
+		Thread t = new Thread() {
+			public void run() {
+				Runnable r = new Runnable() {
+					public void run() {
+						KeyboardFocusManager focusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+						focusManager.addPropertyChangeListener(SINGLETON);
+					}
+				};
+				EventQueue.invokeLater(r);				
+			}
+		};
+		t.start();
+		
+		Toolkit.getDefaultToolkit().addAWTEventListener(SINGLETON, AWTEvent.MOUSE_EVENT_MASK);
+	}
+	
+	public static ViewListener getInstance() {
+		return SINGLETON;
+	}
+	
+	private ViewListener() {
+	}
+	
+	public void eventDispatched(AWTEvent event) {
+		//catch all mousePressed events
+		if(event.getID()!=MouseEvent.MOUSE_PRESSED)
+			return;
+
+		MouseEvent evt = (MouseEvent)event;
+		Component c = (Component)evt.getSource();
+		
+		// check to see if the event was targeted at the deepest component at the current
+		// mouse loaction
+		Container  container = c instanceof Container? (Container)c: null;
+		if(container!=null && container.getComponentCount()>1) {
+			// if not, find the deepest component
+			Point p = evt.getPoint();
+			c = SwingUtilities.getDeepestComponentAt(c, p.x, p.y);
+		}
+		
+		// request activation of the view that encloses this component
+		ViewTracker.requestViewActivation(c);
+	}
+	
+	public void propertyChange(PropertyChangeEvent evt) {
+		String pName = evt.getPropertyName();
+		if(!PROP_EVENTS.contains(pName))
+			return;
+		
+		Component oldVal = toComponent(evt.getOldValue());
+		Component newVal = toComponent(evt.getNewValue());
+		boolean switchTo = newVal!=null;
+		
+		if(ACTIVE_WINDOW.equals(pName))
+			handleWindowChange(evt, oldVal, newVal, switchTo);
+		else
+			handleFocusChange(evt, oldVal, newVal, switchTo);
+	}
+	
+	private void handleWindowChange(PropertyChangeEvent evt, Component oldVal, Component newVal, boolean activate) {
+		// notify the ViewTracker of the window change
+		ViewTracker.windowActivated(newVal);
+		
+		Component srcComponent = activate? newVal: oldVal;
+		ViewTracker tracker = ViewTracker.getTracker(srcComponent);
+		if(tracker!=null)
+			tracker.setActive(activate);
+	}
+	
+	private void handleFocusChange(PropertyChangeEvent evt, Component oldVal, Component newVal, boolean switchTo) {
+		if(!switchTo)
+			return;
+		
+		if(newVal instanceof JTabbedPane)
+			newVal = ((JTabbedPane)newVal).getSelectedComponent();
+		activateComponent(newVal);
+	}
+	
+	private void activateComponent(Component c) {
+		View view = c instanceof View? (View)c: (View)SwingUtilities.getAncestorOfClass(View.class, c);
+		if(view==null)
+			return;
+
+		ViewTracker tracker = ViewTracker.getTracker(view);
+		if(tracker!=null) {
+			tracker.setActive(view);
+		}
+	}
+	
+	private static Component toComponent(Object obj) {
+		return obj instanceof Component? (Component)obj: null;
+	}
+
+	public void stateChanged(ChangeEvent e) {
+		Object obj = e.getSource();
+		if(obj instanceof JTabbedPane) {
+			JTabbedPane pane = (JTabbedPane)obj;
+			Component c = pane.getSelectedComponent();
+			if(c instanceof View)
+				activateComponent(c);
+		}
+	}
+
+}
