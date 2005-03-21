76e1ed87e78eba8a89279c64a5cda772942ee4e6
cro <no_author@no_author>
2005-03-21 10:21:27 +0000

    PainterResource is a PropertySet now
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@148 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/view/org/flexdock/view/ext/PainterResource.java
+++ src/java/view/org/flexdock/view/ext/PainterResource.java
@@ -1,91 +1,73 @@
 /*
  * Created on 18.03.2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package org.flexdock.view.ext;
 
 import java.awt.Color;
 
-/**
- * @author cro
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-public class PainterResource {
+import org.flexdock.view.plaf.PropertySet;
 
-    private String classname;
-    
-    private Color bgColor;
-    private Color bgColorActiv;
-    
-    private Color firstColor;
-    private Color firstColorActiv;
-    
+/**
+ * @author Claudio Romano
+ */
+public class PainterResource extends PropertySet{
+
+    public static final String CLASSNAME = "classname";
+    public static final String BACKGROUND_COLOR = "bgcolor";
+	public static final String BACKGROUND_COLOR_ACTIVE = "bgcolor.active";
     
     
     /**
      * @return Returns the bgColor.
      */
     public Color getBgColor() {
-        return bgColor;
+        return getColor( BACKGROUND_COLOR);
     }
+    
     /**
      * @param bgColor The bgColor to set.
      */
     public void setBgColor(Color bgColor) {
-        this.bgColor = bgColor;
+        setProperty( BACKGROUND_COLOR, bgColor);
     }
+    
     /**
      * @return Returns the bgColorActiv.
      */
     public Color getBgColorActiv() {
-        return bgColorActiv;
+        return getColor( BACKGROUND_COLOR_ACTIVE);
     }
+    
     /**
      * @param bgColorActiv The bgColorActiv to set.
      */
     public void setBgColorActiv(Color bgColorActiv) {
-        this.bgColorActiv = bgColorActiv;
+        setProperty( BACKGROUND_COLOR_ACTIVE, bgColorActiv);
     }
     
-    
-    /**
-     * @return Returns the firstColor.
-     */
-    public Color getFirstColor() {
-        return firstColor;
-    }
-    /**
-     * @param firstColor The firstColor to set.
-     */
-    public void setFirstColor(Color firstColor) {
-        this.firstColor = firstColor;
-    }
-    /**
-     * @return Returns the firstColorActiv.
-     */
-    public Color getFirstColorActiv() {
-        return firstColorActiv;
-    }
-    /**
-     * @param firstColorActiv The firstColorActiv to set.
-     */
-    public void setFirstColorActiv(Color firstColorActiv) {
-        this.firstColorActiv = firstColorActiv;
-    }
     /**
      * @return Returns the painter.
      */
     public String getClassname() {
-        return classname;
+        return getString( CLASSNAME);
     }
     /**
      * @param painter The painter to set.
      */
     public void setClassname(String painter) {
-        this.classname = painter;
+        setProperty(CLASSNAME, painter);
+    }
+    
+    
+    public Class getImplClass() {  
+        String className = getString(CLASSNAME);  
+        try {
+            return resolveClass(getString( CLASSNAME));
+        }
+        catch( Exception e) {
+            e.printStackTrace();
+        }
+        
+        return null;
     }
 }
--- src/java/view/org/flexdock/view/plaf/resources/PainterResourceHandler.java
+++ src/java/view/org/flexdock/view/plaf/resources/PainterResourceHandler.java
@@ -16,25 +16,21 @@ import org.flexdock.view.plaf.PropertySet;
 public class PainterResourceHandler extends ResourceHandler {
     public static final String PAINTER_RESOURCE_KEY = "painter-resource";
     
-    public static final String CLASSNAME = "classname";
-    public static final String BACKGROUND_COLOR = "bgcolor";
-	public static final String BACKGROUND_COLOR_ACTIVE = "bgcolor.active";
-    
     public Object getResource(String stringValue) {
         PropertySet propertySet = Configurator.getProperties(stringValue, PAINTER_RESOURCE_KEY);
         PainterResource pr = createResource( propertySet);
         
-        Painter p = createPainter( pr.getClassname());
+        Painter p = createPainter( pr.getImplClass());
         p.setPainterResource( pr);
 		return p;
 	}
     
-    private static Painter createPainter(String className) {
-		if(Configurator.isNull(className))
+    private static Painter createPainter(Class clazz) {
+        
+		if(clazz ==null)
 			return null;
-
+		
 		try {
-			Class clazz = Class.forName(className);
 			return (Painter)clazz.newInstance();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -42,12 +38,9 @@ public class PainterResourceHandler extends ResourceHandler {
 		}
 	}
     
-    private static PainterResource createResource(PropertySet properties) {
+    private static PainterResource createResource(PropertySet properties) {        
 		PainterResource painterResource = new PainterResource();
-		painterResource.setClassname( properties.getString(CLASSNAME));
-		painterResource.setBgColor(properties.getColor(BACKGROUND_COLOR));
-		painterResource.setBgColorActiv(properties.getColor(BACKGROUND_COLOR_ACTIVE));
-
+		painterResource.setAll( properties);
 		return painterResource;		
 	}
 }
