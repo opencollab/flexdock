1ca85ad85448330930e3950886023580e6996020
cro <no_author@no_author>
2005-03-22 22:59:41 +0000

    add new eclipse v. 2 theme
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@156 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/view/org/flexdock/view/plaf/theme/eclipse2/EclipseGradientPainter.java
@@ -0,0 +1,32 @@
+/*
+ * Created on 22.03.2005
+ */
+package org.flexdock.view.plaf.theme.eclipse2;
+
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+import org.flexdock.view.ext.DefaultPainter;
+
+/**
+ * @author Claudio Romano
+ */
+public class EclipseGradientPainter extends DefaultPainter {
+    
+    public void paint(Graphics g, int width, int height, boolean active, JComponent titlebar) {
+        float center = width / 1.3f;
+        
+        GradientPaint firstHalf;
+        if( active)
+        	firstHalf = new GradientPaint(0, 0, getBackgroundColorActive(), center, 0, getBackgroundColorInactive());
+        else
+            firstHalf = new GradientPaint(0, 0, getBackgroundColorInactive(), center, 0, getBackgroundColorInactive());
+    
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint(firstHalf);
+        g2.fillRect(0, 0, width, height);
+    }
+}
