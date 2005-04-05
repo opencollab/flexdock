1592fb9912e073bc51817618a043ccb49e1922be
Marius <no_author@no_author>
2005-04-05 00:02:33 +0000

    added new source directory /src/java/perspective
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@181 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/perspective/org/flexdock/perspective/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Apr 4, 2005
+ */
+package org.flexdock.perspective;
+
+/**
+ * @author Christopher Butler
+ */
+public class HelloWorld {
+	// TODO: Delete this class once there are more CVS checkins.  
+	// This is only a placeholder for the /src/java/perspective directory, since
+	// CVS won't let you commit empty directories.
+}
