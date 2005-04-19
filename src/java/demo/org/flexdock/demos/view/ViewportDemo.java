ac59021853579cad4c0fee8aec8b6ef566840b95
Marius <no_author@no_author>
2005-04-19 04:39:20 +0000

    updated to use common VSNetStartPage class
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@260 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/demo/org/flexdock/demos/view/ViewportDemo.java
+++ src/java/demo/org/flexdock/demos/view/ViewportDemo.java
@@ -6,13 +6,10 @@ package org.flexdock.demos.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -22,23 +19,19 @@ import java.util.Collections;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.Icon;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
+import org.flexdock.demos.util.VSNetStartPage;
 import org.flexdock.docking.Dockable;
 import org.flexdock.docking.DockingManager;
 import org.flexdock.docking.DockingPort;
-import org.flexdock.util.ResourceManager;
 import org.flexdock.util.SwingUtility;
 import org.flexdock.view.View;
 import org.flexdock.view.Viewport;
@@ -53,12 +46,19 @@ public class ViewportDemo extends JFrame {
 		SwingUtility.setPlaf("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		//		SwingUtility.setPlaf("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
 		//		SwingUtility.setPlaf("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				startup();
+			}
+		});
+	}
+	
+	private static void startup() {
 		JFrame f = new ViewportDemo();
 		f.setSize(800, 600);
 		SwingUtility.centerOnScreen(f);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
+		f.setVisible(true);		
 	}
 
 	public ViewportDemo() {
@@ -124,106 +124,11 @@ public class ViewportDemo extends JFrame {
 	}
 
 	private View createStartPage() {
-		String name = "Start Page";
+
 		String id = "startPage";
-		final Icon miscIcons = ResourceManager.createIcon("org/flexdock/demos/view/ms_misc_icons001.png");
-		final Image tabsImg = ResourceManager.createImage("org/flexdock/demos/view/ms_tabs001.png");
-		final Color tabRunBG = new Color(247, 243, 233);
-		final Color contentBG1 = new Color(246, 246, 246);
-		final Color contentBG2 = new Color(102, 153, 204);
-		final Color tableBG1 = new Color(154, 154, 143);
-		final Font labelFont = new Font("Dialog", Font.BOLD, 11);
-		final JPanel page = new JPanel(new BorderLayout(0, 0));
 
-		final JButton button1 = new JButton("New Project");
-		final JButton button2 = new JButton("Open Project");
-
-		final JPanel table = new JPanel() {
-			protected void paintComponent(Graphics g) {
-				g.setColor(tableBG1);
-				g.fillRect(0, 0, getWidth(), 20);
-				g.setColor(page.getBackground());
-				g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
-				g.setColor(Color.BLACK);
-				g.setFont(labelFont);
-				g.drawString("Name", 5, 15);
-				g.drawString("Modified", 350, 15);
-			}
-		};
-
-		JPanel content = new JPanel(null) {
-			public void doLayout() {
-				int tableH = getHeight() - 120 - 55;
-				tableH = Math.max(tableH, 25);
-				table.setBounds(12, 120, 475, tableH);
-
-				int buttonY = 120 + tableH + 18;
-				Dimension d = button1.getPreferredSize();
-				button1.setBounds(12, buttonY, d.width, d.height);
-				button2.setBounds(24 + d.width, buttonY, button2.getPreferredSize().width, d.height);
-			}
-
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				int w = getWidth();
-
-				Color origC = g.getColor();
-				Font origF = g.getFont();
-
-				g.setColor(Color.WHITE);
-				g.fillRect(0, 0, w, getHeight());
-
-				g.setColor(contentBG1);
-				g.fillRect(0, 0, w, 48);
-				g.setColor(contentBG2);
-				g.fillRect(0, 48, w, 23);
-				g.drawImage(tabsImg, 0, 0, null, this);
-
-				g.setColor(Color.BLACK);
-				g.setFont(labelFont);
-				g.drawString("Open an Existing Project", 12, 100);
-
-				g.setFont(origF);
-				g.setColor(origC);
-			}
-		};
-		content.add(table);
-		content.add(button1);
-		content.add(button2);
-
-		JTabbedPane tabPane = new JTabbedPane(SwingConstants.TOP) {
-			protected void paintComponent(Graphics g) {
-				Color orig = g.getColor();
-				Rectangle tabBounds = getBoundsAt(0);
-				int tabLowerY = tabBounds.y + tabBounds.height;
-
-				g.setColor(tabRunBG);
-				g.fillRect(0, 0, getWidth(), tabLowerY);
-
-				int iconX = getWidth() - miscIcons.getIconWidth();
-				int iconY = (tabLowerY) / 2 - miscIcons.getIconHeight() / 2 + 1;
-				miscIcons.paintIcon(this, g, iconX, iconY);
-
-				g.setColor(orig);
-				super.paintComponent(g);
-
-				g.setColor(Color.WHITE);
-				g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
-				g.drawRect(1, tabLowerY - 1, getWidth() - 3, getHeight() - tabLowerY - 1);
-				g.setColor(orig);
-			}
-		};
-		tabPane.addTab(name, content);
-		tabPane.setBorder(null);
-		page.add(tabPane, BorderLayout.CENTER);
-		page.setBorder(new LineBorder(Color.GRAY, 1));
-
-		View view = new View(id, null, null);
-		view.setTerritoryBlocked(DockingPort.CENTER_REGION, true);
-		view.setTitlebar(null);
-		view.setContentPane(page);
-
-		button1.addActionListener(new ActionListener() {
+		VSNetStartPage page = new VSNetStartPage();
+		page.getNewProjButton().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				
 				JDialog dialog = getSiblingTestDialog();
@@ -235,6 +140,11 @@ public class ViewportDemo extends JFrame {
 				testSiblings();
 			}
 		});
+
+		View view = new View(id, null, null);
+		view.setTerritoryBlocked(DockingPort.CENTER_REGION, true);
+		view.setTitlebar(null);
+		view.setContentPane(page);
 
 		return view;
 	}
