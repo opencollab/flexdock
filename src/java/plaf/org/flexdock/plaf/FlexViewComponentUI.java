5cff862416ecda1716c89aa15847fe65216ad64f
Marius <no_author@no_author>
2005-04-23 18:14:50 +0000

    moved plaf resources from /resources/view to /resources/plaf
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@282 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/plaf/org/flexdock/plaf/FlexViewComponentUI.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Mar 1, 2005
+ */
+package org.flexdock.plaf;
+
+import javax.swing.plaf.ComponentUI;
+
+/**
+ * @author Christopher Butler
+ */
+public abstract class FlexViewComponentUI extends ComponentUI implements IFlexViewComponentUI {
+	protected PropertySet creationParameters;
+
+	public PropertySet getCreationParameters() {
+		return creationParameters;
+	}
+
+	public void setCreationParameters(PropertySet creationParameters) {
+		this.creationParameters = creationParameters;
+		initializeCreationParameters();
+	}
+	
+	public abstract void initializeCreationParameters();
+}
---
+++ src/java/plaf/org/flexdock/plaf/IFlexViewComponentUI.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Mar 1, 2005
+ */
+package org.flexdock.plaf;
+
+/**
+ * @author Christopher Butler
+ */
+public interface IFlexViewComponentUI extends XMLConstants {
+	public static final String ICON_RESOURCE = "flexdock.button.icon.resource";
+	
+	public PropertySet getCreationParameters();
+	public void setCreationParameters(PropertySet creationParameters);
+	public void initializeCreationParameters();
+}
