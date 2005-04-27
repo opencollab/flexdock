f001e12b112c764b9fda730a1b6664344845f505
Marius <no_author@no_author>
2005-04-27 06:52:55 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@312 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/view/org/flexdock/view/actions/ViewAction.java
+++ src/java/view/org/flexdock/view/actions/ViewAction.java
@@ -3,23 +3,32 @@
  */
 package org.flexdock.view.actions;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.SwingUtilities;
 
 import org.flexdock.view.View;
 
 /**
  * @author Christopher Butler
  */
-public class ViewAction extends AbstractAction {
-	protected View m_View;
+public abstract class ViewAction extends AbstractAction {
+	public static final ViewAction EMPTY_ACTION = createDefault();
 	
-	public ViewAction(View view) {
-		m_View = view;
+	public abstract void actionPerformed(ActionEvent e);
+	
+	private static ViewAction createDefault() {
+		return new ViewAction() {
+			public void actionPerformed(ActionEvent ae) {
+				
+			}
+		};
 	}
 	
-	public void actionPerformed(ActionEvent e) {
-		// noop
+	protected View getView(ActionEvent evt) {
+		Component c = (Component)evt.getSource();
+		return (View)SwingUtilities.getAncestorOfClass(View.class, c);
 	}
 }
