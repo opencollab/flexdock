eb0c18deb74804a070d24e8c3a8ef50d1a997f25
Marius <no_author@no_author>
2005-04-29 01:42:37 +0000

    added new fields from SwingConstants
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@339 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/core/org/flexdock/util/DockingConstants.java
+++ src/java/core/org/flexdock/util/DockingConstants.java
@@ -3,6 +3,9 @@
  */
 package org.flexdock.util;
 
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
+
 /**
  * @author Christopher Butler
  */
@@ -13,4 +16,14 @@ public interface DockingConstants {
 	
 	public static final String PIN_ACTION = "pin";
 	public static final String CLOSE_ACTION = "close";
+	
+	public static final int TOP = SwingConstants.TOP;
+	public static final int LEFT = SwingConstants.LEFT;
+	public static final int BOTTOM = SwingConstants.BOTTOM;
+	public static final int RIGHT = SwingConstants.RIGHT;
+	public static final int CENTER = SwingConstants.CENTER;
+	
+	public static final int HORIZONTAL = JSplitPane.HORIZONTAL_SPLIT;
+	public static final int VERTICAL = JSplitPane.VERTICAL_SPLIT;
+	
 }
