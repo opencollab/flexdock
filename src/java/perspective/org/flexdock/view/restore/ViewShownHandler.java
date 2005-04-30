15179b24793b4625c68fc80530460e48858a2198
winnetou25 <no_author@no_author>
2005-04-30 10:55:09 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@345 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/perspective/org/flexdock/view/restore/ViewShownHandler.java
+++ src/java/perspective/org/flexdock/view/restore/ViewShownHandler.java
@@ -8,6 +8,7 @@ package org.flexdock.view.restore;
 
 import java.util.Map;
 
+import org.flexdock.dockbar.DockbarManager;
 import org.flexdock.docking.Dockable;
 import org.flexdock.docking.DockingManager;
 import org.flexdock.util.SwingUtility;
@@ -20,7 +21,11 @@ import org.flexdock.view.tracking.ViewTracker;
 public class ViewShownHandler implements ShowViewHandler {
 
 	public boolean showView(View view, Map context) {
-
+		if (view.isMinimized()) {
+			DockbarManager.getCurrent().setActiveDockable(view);
+			//DockingManager.setMinimized(view, false);
+			return true;
+		}
 		if (DockingManager.isDocked((Dockable) view)) {
 			ViewTracker.requestViewActivation(view);
 			SwingUtility.focus(view);
