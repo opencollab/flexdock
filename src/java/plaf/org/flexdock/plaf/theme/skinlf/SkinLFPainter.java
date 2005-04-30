ec9d7147c8b17986f72e5a39aa7bc28d69623d3a
cro <no_author@no_author>
2005-04-30 23:43:47 +0000

    add the skinlookandfeel theme
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@348 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/plaf/org/flexdock/plaf/theme/skinlf/SkinLFPainter.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 15.04.2005
+ */
+package org.flexdock.plaf.theme.skinlf;
+
+import java.awt.Graphics;
+
+import javax.swing.JComponent;
+
+import org.flexdock.plaf.resources.paint.DefaultPainter;
+
+import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
+
+/**
+ * @author Claudio Romano
+ */
+public class SkinLFPainter extends DefaultPainter { 
+    public void paint(Graphics g, int width, int height, boolean active, JComponent titlebar) {
+        SkinLookAndFeel.getSkin().getFrame().paintTop(g, titlebar, active, "");
+    }
+}
---
+++ src/java/plaf/org/flexdock/plaf/theme/skinlf/SkinLFTitlebarUI.java
@@ -0,0 +1,30 @@
+/*
+ * Created on 17.04.2005
+ */
+package org.flexdock.plaf.theme.skinlf;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import org.flexdock.plaf.theme.TitlebarUI;
+import org.flexdock.view.Titlebar;
+
+import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
+
+/**
+ * @author Claudio Romano
+ */
+public class SkinLFTitlebarUI extends TitlebarUI {
+    
+    protected void paintBackground(Graphics g, Titlebar titlebar) {
+        Rectangle paintArea = getPaintRect(titlebar);
+        g.translate(paintArea.x, paintArea.y);
+        SkinLookAndFeel.getSkin().getFrame().paintTop(g, titlebar, titlebar.isActive(), titlebar.getText());
+        g.translate(-paintArea.x, -paintArea.y);
+    }
+    
+    
+    public int getDefaultHeight() {
+        return SkinLookAndFeel.getSkin().getFrame().getTopPreferredSize().height;
+    }
+}
