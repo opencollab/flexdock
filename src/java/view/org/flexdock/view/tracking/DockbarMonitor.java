10bd9f8c237be19d35a0fd40a567039c97279a30
Marius <no_author@no_author>
2005-05-01 21:02:17 +0000

    added new methods for DockbarListener updates. implemented minimizedStarted() to block minimization on floating views
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@354 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/view/org/flexdock/view/tracking/DockbarMonitor.java
+++ src/java/view/org/flexdock/view/tracking/DockbarMonitor.java
@@ -6,7 +6,10 @@ package org.flexdock.view.tracking;
 import org.flexdock.dockbar.event.DockbarEvent;
 import org.flexdock.dockbar.event.DockbarListener;
 import org.flexdock.docking.Dockable;
+import org.flexdock.docking.DockingManager;
+import org.flexdock.docking.DockingPort;
 import org.flexdock.view.View;
+import org.flexdock.view.floating.FloatingViewport;
 
 /**
  * @author Christopher Butler
@@ -30,5 +33,19 @@ public class DockbarMonitor implements DockbarListener {
 	private View getView(DockbarEvent evt) {
 		Dockable d = (Dockable)evt.getSource();
 		return d instanceof View? (View)d: null;
+	}
+	
+	public void minimizeStarted(DockbarEvent evt) {
+		View view = getView(evt);
+		if(view!=null) {
+			DockingPort port = DockingManager.getDockingPort((Dockable)view);
+			// block minimization on floating views
+			if(port instanceof FloatingViewport)
+				evt.consume();
+		}
+	}
+	
+	public void minimizeCompleted(DockbarEvent evt) {
+		
 	}
 }
