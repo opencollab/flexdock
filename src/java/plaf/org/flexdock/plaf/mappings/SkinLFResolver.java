53390c6990d621aa646363aefb010883bda92e88
winnetou25 <no_author@no_author>
2005-05-01 19:40:15 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@351 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/plaf/org/flexdock/plaf/mappings/SkinLFResolver.java
+++ src/java/plaf/org/flexdock/plaf/mappings/SkinLFResolver.java
@@ -1,30 +1,30 @@
-///*
-// * Created on Feb 27, 2005
-// */
-//package org.flexdock.plaf.mappings;
-//
-//import javax.swing.UIManager;
-//
-//import com.l2fprod.gui.plaf.skin.Skin;
-//import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
-//
-///**
-// * @author Christopher Butler
-// */
-//public class SkinLFResolver extends RefResolver {
-//	
-//	public String getRef(String plaf) {
-//		Skin skin = SkinLookAndFeel.getSkin();
-//		String skinName = skin==null? null: skin.getClass().getName();
-//		
-//		// redirect to the mapping for the skin, instead of the plaf itself
-//		String view = PlafMappingFactory.getPlafReference(skinName);
-//		return view==null? getDefaultRef(): view;
-//	}
-//	
-//	public String getDefaultRef() {
-//		String systemPlaf = UIManager.getSystemLookAndFeelClassName();
-//		return PlafMappingFactory.getPlafReference(systemPlaf);
-//	}
-//
-//}
+/*
+ * Created on Feb 27, 2005
+ */
+package org.flexdock.plaf.mappings;
+
+import javax.swing.UIManager;
+
+import com.l2fprod.gui.plaf.skin.Skin;
+import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
+
+/**
+ * @author Christopher Butler
+ */
+public class SkinLFResolver extends RefResolver {
+	
+	public String getRef(String plaf) {
+		Skin skin = SkinLookAndFeel.getSkin();
+		String skinName = skin==null? null: skin.getClass().getName();
+		
+		// redirect to the mapping for the skin, instead of the plaf itself
+		String view = PlafMappingFactory.getPlafReference(skinName);
+		return view==null? getDefaultRef(): view;
+	}
+	
+	public String getDefaultRef() {
+		String systemPlaf = UIManager.getSystemLookAndFeelClassName();
+		return PlafMappingFactory.getPlafReference(systemPlaf);
+	}
+
+}
