647066880e3bac6551c774263d339d1d7e183b50
winnetou25 <no_author@no_author>
2005-06-04 16:21:45 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@452 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/perspective/org/flexdock/perspective/restore/handlers/RestorationHandler.java
+++ src/java/perspective/org/flexdock/perspective/restore/handlers/RestorationHandler.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (c) 2005 FlexDock Development Team. All rights reserved. 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the 
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE.
+ */
 package org.flexdock.perspective.restore.handlers;
 
 import java.util.Map;
@@ -6,11 +24,13 @@ import org.flexdock.docking.Dockable;
 import org.flexdock.docking.state.DockingState;
 
 /**
- *
- * @author Mateusz Szczap 
+ * Created on 2005-06-03
+ * 
+ * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
+ * @version $Id: RestorationHandler.java,v 1.2 2005-06-04 16:21:45 winnetou25 Exp $
  */
 public interface RestorationHandler {
 
-	boolean restore(Dockable dockable, DockingState info, Map context);
+	boolean restore(Dockable dockable, DockingState dockingState, Map context);
 	
 }
