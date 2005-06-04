a6798255ab0436b0e9e603a53f3899aabb586ffd
winnetou25 <no_author@no_author>
2005-06-04 14:00:29 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@446 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/perspective/org/flexdock/perspective/persist/xml/PointSerializer.java
+++ src/java/perspective/org/flexdock/perspective/persist/xml/PointSerializer.java
@@ -1,8 +1,20 @@
-/*
- * Created on 2005-06-03
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+/* 
+ * Copyright (c) 2005 FlexDock Development Team. All rights reserved. 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the 
+ * Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE.
  */
 package org.flexdock.perspective.persist.xml;
 
@@ -12,10 +24,10 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
- * @author mati
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Created on 2005-06-03
+ * 
+ * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
+ * @version $Id: PointSerializer.java,v 1.2 2005-06-04 14:00:28 winnetou25 Exp $
  */
 public class PointSerializer implements ISerializer {
 
