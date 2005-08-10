1dcf221df5c774db60611bc45dc730c54faf1560
winnetou25 <no_author@no_author>
2005-08-10 20:26:30 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@600 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/view/org/flexdock/view/actions/ActionFactory.java
@@ -0,0 +1,17 @@
+package org.flexdock.view.actions;
+
+public class ActionFactory {
+    
+    public static DefaultCloseAction createCloseAction() {
+        return new DefaultCloseAction();
+    }
+    
+    public static DefaultDisplayAction createDisplayAction() {
+        return new DefaultDisplayAction();
+    }
+    
+    public static DefaultPinAction createPinAction() {
+        return new DefaultPinAction();
+    }
+    
+}
