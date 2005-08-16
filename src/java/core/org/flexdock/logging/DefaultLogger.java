78e10195c8bb838fb9c283e7b4b2b35a35586f08
kay <no_author@no_author>
2005-08-16 17:48:27 +0000

    Discussion in dev list about Logging
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@626 466b13bf-535e-ee63-ab3f-edaa65664d73

---
+++ src/java/core/org/flexdock/logging/DefaultLogger.java
@@ -0,0 +1,20 @@
+package org.flexdock.logging;
+
+/**
+ * Default logging implementation for flexdock.<br>
+ * All log statements are sent to <code>System.err</code>.
+ * Since this class inherits from <code>SimpleLogger</code>,
+ * only warnings and errors are logged, no debug messages.
+ */
+class DefaultLogger extends SimpleLogger {
+
+    public void log(String message, Throwable t) {
+        if (message != null) {
+            System.err.println(message);
+        }
+        if (t != null) {
+            t.printStackTrace();
+        }
+    }
+
+}
