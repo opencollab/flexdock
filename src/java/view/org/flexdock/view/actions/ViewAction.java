44f89cc9c68bd927dfe8675e57ac510c940a8fbf
kschaefe <no_author@no_author>
2007-03-02 18:54:18 +0000

    Deprecate unused action.
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@749 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/view/org/flexdock/view/actions/ViewAction.java
+++ src/java/view/org/flexdock/view/actions/ViewAction.java
@@ -16,45 +16,47 @@ import org.flexdock.view.View;
  * @author Christopher Butler
  */
 public abstract class ViewAction extends AbstractAction {
-
+    /**
+     * @deprecated unused
+     */
     public static final ViewAction EMPTY_ACTION = createDefault();
-    
-	protected String viewId;
-	
-	protected View getView(ActionEvent evt) {
-		View view = viewId==null? null: View.getInstance(viewId);
-		if(view==null) {
-			Component c = (Component)evt.getSource();
-			view = (View)SwingUtilities.getAncestorOfClass(View.class, c);			
-		}
-		return view;
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		View view = getView(e);
-		actionPerformed(view, e);
-	}
-	
-	public abstract void actionPerformed(View view, ActionEvent evt);
-	
-	private static ViewAction createDefault() {
-		return new ViewAction() {
-			public void actionPerformed(View view, ActionEvent evt) {
-			    
-			}
-		};
-	}
-	
-	public ButtonModel createButtonModel() {
-		return null;
-	}
-	
-	public String getViewId() {
-		return viewId;
-	}
-	
-	public void setViewId(String viewId) {
-		this.viewId = viewId;
-	}
-    
+
+    protected String viewId;
+
+    protected View getView(ActionEvent evt) {
+        View view = viewId == null ? null : View.getInstance(viewId);
+        if (view == null) {
+            Component c = (Component) evt.getSource();
+            view = (View) SwingUtilities.getAncestorOfClass(View.class, c);
+        }
+        return view;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        View view = getView(e);
+        actionPerformed(view, e);
+    }
+
+    public abstract void actionPerformed(View view, ActionEvent evt);
+
+    private static ViewAction createDefault() {
+        return new ViewAction() {
+            public void actionPerformed(View view, ActionEvent evt) {
+
+            }
+        };
+    }
+
+    public ButtonModel createButtonModel() {
+        return null;
+    }
+
+    public String getViewId() {
+        return viewId;
+    }
+
+    public void setViewId(String viewId) {
+        this.viewId = viewId;
+    }
+
 }
