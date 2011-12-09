2d20867cf38adc65b55d5410e350ed17b3b3e626
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2011-12-09 10:12:20 +0000

    This version is tagged 1.0 in order to show that it reached a stable status. It has been used by Scilab for years without any big issues.
    git-svn-id: svn+ssh://svn.java.net/flexdock~svn/trunk@796 466b13bf-535e-ee63-ab3f-edaa65664d73

--- src/java/core/org/flexdock/util/Utilities.java
+++ src/java/core/org/flexdock/util/Utilities.java
@@ -31,7 +31,7 @@ public class Utilities {
     /**
      * A String representing the flexdock version. This constant is a string.
      */
-    public static final String VERSION = "0.5.4";
+    public static final String VERSION = "1.0";
 
     private Utilities() {
         // does nothing
